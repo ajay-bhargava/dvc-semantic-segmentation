e57f64db92c2176a24d2e1d16cbd58d2215d9a2c
Ajay Bhargava <bhargava.ajay@gmail.com>
2023-02-12 16:43:39 -0500

    removing transitory scripts

--- scripts.sh
+++
@@ -1,8 +0,0 @@
-dvc import-url s3://dvc-datasets/semantic-segmentation/tracked/pets-dataset/labelstudio-pets-dataset-export.json \
-    data/labelstudio-pets-dataset.json \
-    --to-remote \
-    --remote pets-remote \
-    --version-aware \
-    --desc "LabelStudio export of Pets Dataset for Semantic Segmentation" \
-    --type "json" \
-    --meta created="2023-02-12"
