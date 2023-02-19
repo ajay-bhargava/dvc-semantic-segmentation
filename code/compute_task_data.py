8a17587687a23a55bf8d736f02ceb89093c9f59b
Ajay Bhargava <bhargava.ajay@gmail.com>
2023-02-18 19:45:26 -0500

    finished compute task data script.

---
+++ code/compute_task_data.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import numpy as np, boto3
+from helpers.task_parsers import LabelStudioData, LabelStudioDataSet
+from sklearn.model_selection import train_test_split
+from argparse import ArgumentParser
+from tqdm import tqdm
+
+S3 = boto3.client('s3')
+
+def argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--dataset', type=str, required=True, help='Input LabelStudio Dataset Path')
+    parser.add_argument('--output', type=str, required=True, help='Output directory')
+    return parser
+
+def compute_task_data() -> None:
+    '''
+    Performs the derivation of test and train numpy arrays from the LabelStudio dataset
+    '''
+    # Parse the arguments
+    ARGUMENTS = argument_parser().parse_args()
+
+    # Load the dataset and split it into train and test
+    dataset = LabelStudioDataSet(Path(ARGUMENTS.dataset).resolve())
+    train, test = train_test_split(dataset, test_size=0.2, random_state=42, shuffle=True)
+
+    # Split the Dataset
+    output_root = Path(ARGUMENTS.output).resolve()
+    train, test = train_test_split(dataset, test_size=0.2, random_state=42, shuffle=True)
+
+    # Build the output test and train directory
+    Path(output_root / 'train').mkdir(parents=True, exist_ok=True)
+    Path(output_root / 'test').mkdir(parents=True, exist_ok=True)
+    
+    # Iterate through the data. Save the image and label. 
+    for dataset, name in zip([train, test], ['train', 'test']):
+        counter = 0
+        pbar = tqdm(dataset, desc='Processing {} Data.'.format(name), total=len(dataset), unit = 'image', leave=False)
+        for data in dataset:
+            # Derive the image and label
+            datum = LabelStudioData(data).task_details
+            for element in datum:
+                counter += 1
+                # Get key value pairs from structured dictionary
+                mask = element['array']
+                uri = element['uri']
+
+                # Save the image and label
+                np.save(Path(output_root / name / '{}.npy'.format(str(counter).zfill(4))), mask)
+                jpeg_path = str(Path(output_root / name / '{}.jpg'.format(str(counter).zfill(4))).resolve())
+                S3.download_file(uri[0], uri[1], jpeg_path)
+            
+if __name__ == '__main__':
+    compute_task_data()
+    print('Finished computing data.')
---
+++ code/helpers/task_parsers.py
@@ -0,0 +1,63 @@
+import dateutil.parser as DateParser
+from label_studio_converter.brush import decode_rle as Decode
+import json, numpy as np 
+
+def Split_S3(s3_path):
+    path_parts=s3_path.replace("s3://","").split("/")
+    bucket=path_parts.pop(0)
+    key="/".join(path_parts)
+    return bucket, key
+
+class LabelStudioDataSet:
+    def __init__(self, path) -> None:
+        with open(path) as f:
+            self.dataset = json.load(f)
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        return self.dataset[index]
+
+    def __iter__(self):
+        for item in self.dataset:
+            yield item
+
+class LabelStudioData:
+    def __init__(self, task) -> None:
+        self.task = task
+        self.latest_annotation = self.resolve_latest_annotation()
+        self.task_details = self.get_task_details()
+        
+    def __len__(self):
+        return len(self.task['annotations'])
+    
+    def resolve_latest_annotation(self) -> dict:
+        '''
+        Resolve the latest annotation if there is more than one. 
+        '''
+        if len(self) == 1:
+            return self.task['annotations'][0]
+        else:
+            return sorted(self.task['annotations'], key=lambda x: DateParser.parse(x['created_at']))[-1]
+        
+    def get_image_url(self) -> str:
+        return Split_S3(self.task['data']['image'])[0], Split_S3(self.task['data']['image'])[1]
+    
+    def get_task_details(self) -> list:
+        '''
+        Return all the np.ndarrays associated with the RLE's present in the task. 
+        '''
+        output = []
+        counter = 0
+        for result in self.latest_annotation['result']:
+            if result['type'] == 'brushlabels': 
+                #TODO: Add support for other types, and #WARNING: This is a hack.
+                image = Decode(result['value']['rle'])
+                width = result['original_width']
+                height = result['original_height']
+                counter += 1 
+                label = result['value']['brushlabels'][0]
+                image_output = np.reshape(image, [height, width, 4])[...,3]
+                output.append({'array': image_output, 'label': label, 'uri': self.get_image_url()})
+        return output
