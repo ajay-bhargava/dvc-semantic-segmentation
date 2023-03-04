a8768fe19bb6f78f48160fac7271fbc251d766f3
Ajay Bhargava <bhargava.ajay@gmail.com>
2023-03-04 17:17:40 -0500

    see last commit for detailed message: - YAML for hyperparams - ArgParse for data paths

---
+++ code/loaders/configurator.py
@@ -0,0 +1,15 @@
+import yaml
+from functools import reduce
+
+class CONFIGURATOR(object):
+  '''
+  A class that contains all the configuration options for the training process. 
+  We are going to use this in place of a YAML file that acheives the same purpose.
+  '''
+  def __init__(self, path):
+    self.path = path
+    with open(self.path, 'r') as f:
+      self.dictionary = yaml.safe_load(f)
+
+  def retrieve(self, keys, default=None):
+      return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), self.dictionary)
